17714207643457158348ce91e92dc3da235b82fe
Zubair <bairyd@gmail.com>
2021-02-14 23:26:13 +0200

    Added tests for existing service functions

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/CustomerServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/CustomerServiceTest.kt
@@ -4,12 +4,29 @@ import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Customer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class CustomerServiceTest {
+
+    private val validCustomerOneId = 1
+    private val validCustomerTwoId = 2
+    private val validCustomerThreeId = 3
+
     private val dal = mockk<AntaeusDal> {
         every { fetchCustomer(404) } returns null
+        every { fetchCustomer(1) } returns getValidCustomer()
+        every { fetchCustomers() } returns getValidListOfCustomers()
+    }
+
+    private fun getValidCustomer(): Customer {
+        return Customer(validCustomerOneId, Currency.EUR)
+    }
+
+    private fun getValidListOfCustomers(): List<Customer> {
+        return listOf(Customer(validCustomerTwoId, Currency.DKK), Customer(validCustomerThreeId, Currency.GBP))
     }
 
     private val customerService = CustomerService(dal = dal)
@@ -19,5 +36,20 @@ class CustomerServiceTest {
         assertThrows<CustomerNotFoundException> {
             customerService.fetch(404)
         }
+    }
+
+    @Test
+    fun `will return a valid customer when supplied with a matching ID`() {
+        val customer = customerService.fetch(validCustomerOneId)
+        assert(customer.id == 1)
+        assert(customer.currency == Currency.EUR)
+    }
+
+    @Test
+    fun `will return a list of customers if any exist`() {
+        val customers = customerService.fetchAll()
+        assert(customers.size == 2)
+        assert(customers.find { c -> c.id == validCustomerTwoId }?.currency == Currency.DKK)
+        assert(customers.find { c -> c.id == validCustomerThreeId }?.currency == Currency.GBP)
     }
 }
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -4,12 +4,35 @@ import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Money
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 class InvoiceServiceTest {
+
+    private val validInvoiceOneId = 1
+    private val validInvoiceTwoId = 2
+    private val validInvoiceThreeId = 3
+    private val validCustomerId = 1
+
     private val dal = mockk<AntaeusDal> {
         every { fetchInvoice(404) } returns null
+        every { fetchInvoice(validInvoiceOneId) } returns GetValidInvoice()
+        every { fetchInvoices() } returns GetValidInvoices()
+    }
+
+    private fun GetValidInvoice(): Invoice {
+        return Invoice(validInvoiceOneId, validCustomerId, Money(BigDecimal.TEN, Currency.SEK), InvoiceStatus.PAID)
+    }
+
+    private fun GetValidInvoices(): List<Invoice> {
+        return listOf(
+            Invoice(validInvoiceTwoId, validCustomerId, Money(BigDecimal.ZERO, Currency.USD), InvoiceStatus.PENDING),
+            Invoice(validInvoiceThreeId, validCustomerId, Money(BigDecimal.valueOf(150), Currency.EUR), InvoiceStatus.PAID))
     }
 
     private val invoiceService = InvoiceService(dal = dal)
@@ -19,5 +42,35 @@ class InvoiceServiceTest {
         assertThrows<InvoiceNotFoundException> {
             invoiceService.fetch(404)
         }
+    }
+
+    @Test
+    fun `will return an invoice if an Id is supplied and matches a record`() {
+        val invoice = invoiceService.fetch(validInvoiceOneId)
+        assert(invoice.id == validInvoiceOneId)
+        assert(invoice.customerId == validCustomerId)
+        assert(invoice.amount.value == BigDecimal.TEN)
+        assert(invoice.amount.currency == Currency.SEK)
+        assert(invoice.status == InvoiceStatus.PAID)
+    }
+
+    @Test
+    fun `will return a list of invoices if any records exist`() {
+        val invoices = invoiceService.fetchAll()
+        val invoiceTwo = invoices.find { i -> i.id == validInvoiceTwoId }
+
+        assert(invoiceTwo?.id == validInvoiceTwoId)
+        assert(invoiceTwo?.customerId == validCustomerId)
+        assert(invoiceTwo?.amount?.value == BigDecimal.ZERO)
+        assert(invoiceTwo?.amount?.currency == Currency.USD)
+        assert(invoiceTwo?.status == InvoiceStatus.PENDING)
+
+        val invoiceThree = invoices.find { i -> i.id == validInvoiceThreeId }
+
+        assert(invoiceThree?.id == validInvoiceThreeId)
+        assert(invoiceThree?.customerId == validCustomerId)
+        assert(invoiceThree?.amount?.value == BigDecimal.valueOf(150))
+        assert(invoiceThree?.amount?.currency == Currency.EUR)
+        assert(invoiceThree?.status == InvoiceStatus.PAID)
     }
 }
